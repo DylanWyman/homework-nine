512f8a8bc1e53784db847983be1f3af30915e144
DylanWyman <74611535+DylanWyman@users.noreply.github.com>
2021-04-11 19:51:15 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,262 @@
+var userVarName = "Variable";
+var homeContent = `<div class="home-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quaerat nihil a hic assumenda maiores nulla ratione aliquam accusantium? Minus repellendus reprehenderit necessitatibus eveniet, expedita laboriosam doloremque quidem! Expedita dolorum excepturi tempora dolore esse maiores illo, ducimus consequuntur neque hic nisi? Maiores, commodi alias. Commodi, numquam? Cumque, eligendi.
+
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+    
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati vel dignissimos voluptate harum sed debitis reiciendis, dolores eaque odit non, expedita nobis vitae consequuntur, mollitia voluptas sequi? Cum, modi natus voluptatem dolor maiores eum nostrum, ratione provident, quia incidunt adipisci sunt debitis labore iste cupiditate obcaecati blanditiis vel nesciunt suscipit? Error, blanditiis. Nostrum!
+        </p>
+    </div>
+</div>
+</div>
+<div class="home-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ex, explicabo rerum praesentium in sunt incidunt magnam veritatis excepturi similique quidem repellendus repellat corporis nesciunt maxime sapiente quasi, consequuntur aliquam tenetur veniam dicta iste vero? Adipisci, aperiam ducimus asperiores esse vel incidunt. Doloribus, suscipit voluptatibus.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image1">
+
+    </div>
+</div>
+</div>
+<div class="home-frame">
+<div class="inner-frame">
+    <div class="image2">
+
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet molestiae veritatis qui ad omnis minima fugit, quae vero labore, aliquam in aliquid dolorum voluptatum accusamus minus repellat optio modi animi dignissimos nihil, quos repellendus quisquam! Eligendi, enim. Optio, mollitia harum porro sequi, dignissimos illum officia possimus quasi nemo illo eveniet, accusantium consequuntur ad assumenda ea distinctio!
+        </p>
+    </div>
+</div>
+</div>
+<div class="home-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt dolore accusantium laboriosam harum. Enim asperiores eaque alias maiores quae eligendi architecto explicabo! Nesciunt ut repellendus harum cupiditate tempore eveniet quia, non quaerat ipsa assumenda laborum architecto saepe doloremque nostrum animi nulla repudiandae ullam magni veritatis molestias sit.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image3">
+
+    </div>
+</div>
+</div>`;
+var productsContent = `<div class="products-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quaerat nihil a hic assumenda maiores nulla ratione aliquam accusantium? Minus repellendus reprehenderit necessitatibus eveniet, expedita laboriosam doloremque quidem! Expedita dolorum excepturi tempora dolore esse maiores illo, ducimus consequuntur neque hic nisi? Maiores, commodi alias. Commodi, numquam? Cumque, eligendi.
+
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+    
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati vel dignissimos voluptate harum sed debitis reiciendis, dolores eaque odit non, expedita nobis vitae consequuntur, mollitia voluptas sequi? Cum, modi natus voluptatem dolor maiores eum nostrum, ratione provident, quia incidunt adipisci sunt debitis labore iste cupiditate obcaecati blanditiis vel nesciunt suscipit? Error, blanditiis. Nostrum!
+        </p>
+    </div>
+</div>
+</div>
+<div class="products-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ex, explicabo rerum praesentium in sunt incidunt magnam veritatis excepturi similique quidem repellendus repellat corporis nesciunt maxime sapiente quasi, consequuntur aliquam tenetur veniam dicta iste vero? Adipisci, aperiam ducimus asperiores esse vel incidunt. Doloribus, suscipit voluptatibus.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image1">
+
+    </div>
+</div>
+</div>
+<div class="products-frame">
+<div class="inner-frame">
+    <div class="image2">
+
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet molestiae veritatis qui ad omnis minima fugit, quae vero labore, aliquam in aliquid dolorum voluptatum accusamus minus repellat optio modi animi dignissimos nihil, quos repellendus quisquam! Eligendi, enim. Optio, mollitia harum porro sequi, dignissimos illum officia possimus quasi nemo illo eveniet, accusantium consequuntur ad assumenda ea distinctio!
+        </p>
+    </div>
+</div>
+</div>
+<div class="products-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt dolore accusantium laboriosam harum. Enim asperiores eaque alias maiores quae eligendi architecto explicabo! Nesciunt ut repellendus harum cupiditate tempore eveniet quia, non quaerat ipsa assumenda laborum architecto saepe doloremque nostrum animi nulla repudiandae ullam magni veritatis molestias sit.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image3">
+
+    </div>
+</div>
+</div>`;
+var aboutContent = `<div class="about-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quaerat nihil a hic assumenda maiores nulla ratione aliquam accusantium? Minus repellendus reprehenderit necessitatibus eveniet, expedita laboriosam doloremque quidem! Expedita dolorum excepturi tempora dolore esse maiores illo, ducimus consequuntur neque hic nisi? Maiores, commodi alias. Commodi, numquam? Cumque, eligendi.
+
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+    
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati vel dignissimos voluptate harum sed debitis reiciendis, dolores eaque odit non, expedita nobis vitae consequuntur, mollitia voluptas sequi? Cum, modi natus voluptatem dolor maiores eum nostrum, ratione provident, quia incidunt adipisci sunt debitis labore iste cupiditate obcaecati blanditiis vel nesciunt suscipit? Error, blanditiis. Nostrum!
+        </p>
+    </div>
+</div>
+</div>
+<div class="about-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ex, explicabo rerum praesentium in sunt incidunt magnam veritatis excepturi similique quidem repellendus repellat corporis nesciunt maxime sapiente quasi, consequuntur aliquam tenetur veniam dicta iste vero? Adipisci, aperiam ducimus asperiores esse vel incidunt. Doloribus, suscipit voluptatibus.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image1">
+
+    </div>
+</div>
+</div>
+<div class="about-frame">
+<div class="inner-frame">
+    <div class="image2">
+
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet molestiae veritatis qui ad omnis minima fugit, quae vero labore, aliquam in aliquid dolorum voluptatum accusamus minus repellat optio modi animi dignissimos nihil, quos repellendus quisquam! Eligendi, enim. Optio, mollitia harum porro sequi, dignissimos illum officia possimus quasi nemo illo eveniet, accusantium consequuntur ad assumenda ea distinctio!
+        </p>
+    </div>
+</div>
+</div>
+<div class="about-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt dolore accusantium laboriosam harum. Enim asperiores eaque alias maiores quae eligendi architecto explicabo! Nesciunt ut repellendus harum cupiditate tempore eveniet quia, non quaerat ipsa assumenda laborum architecto saepe doloremque nostrum animi nulla repudiandae ullam magni veritatis molestias sit.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image3">
+
+    </div>
+</div>
+</div>`;
+var contactContent = `<div class="contact-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quaerat nihil a hic assumenda maiores nulla ratione aliquam accusantium? Minus repellendus reprehenderit necessitatibus eveniet, expedita laboriosam doloremque quidem! Expedita dolorum excepturi tempora dolore esse maiores illo, ducimus consequuntur neque hic nisi? Maiores, commodi alias. Commodi, numquam? Cumque, eligendi.
+
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+    
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati vel dignissimos voluptate harum sed debitis reiciendis, dolores eaque odit non, expedita nobis vitae consequuntur, mollitia voluptas sequi? Cum, modi natus voluptatem dolor maiores eum nostrum, ratione provident, quia incidunt adipisci sunt debitis labore iste cupiditate obcaecati blanditiis vel nesciunt suscipit? Error, blanditiis. Nostrum!
+        </p>
+    </div>
+</div>
+</div>
+<div class="contact-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ex, explicabo rerum praesentium in sunt incidunt magnam veritatis excepturi similique quidem repellendus repellat corporis nesciunt maxime sapiente quasi, consequuntur aliquam tenetur veniam dicta iste vero? Adipisci, aperiam ducimus asperiores esse vel incidunt. Doloribus, suscipit voluptatibus.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image1">
+
+    </div>
+</div>
+</div>
+<div class="contact-frame">
+<div class="inner-frame">
+    <div class="image2">
+
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet molestiae veritatis qui ad omnis minima fugit, quae vero labore, aliquam in aliquid dolorum voluptatum accusamus minus repellat optio modi animi dignissimos nihil, quos repellendus quisquam! Eligendi, enim. Optio, mollitia harum porro sequi, dignissimos illum officia possimus quasi nemo illo eveniet, accusantium consequuntur ad assumenda ea distinctio!
+        </p>
+    </div>
+</div>
+</div>
+<div class="contact-frame">
+<div class="inner-frame">
+    <div class="paragraph">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt dolore accusantium laboriosam harum. Enim asperiores eaque alias maiores quae eligendi architecto explicabo! Nesciunt ut repellendus harum cupiditate tempore eveniet quia, non quaerat ipsa assumenda laborum architecto saepe doloremque nostrum animi nulla repudiandae ullam magni veritatis molestias sit.
+        </p>
+    </div>
+</div>
+<div class="inner-frame">
+    <div class="image3">
+
+    </div>
+</div>
+</div>`;
+
+function init() {
+
+    $("nav a").click(function (e) {
+
+        let btnID = this.id;
+        let contentID = btnID + "Content";
+
+
+        $("#app").html(eval(contentID));
+        //$("#app").prepend(btnID);
+
+    });
+
+}
+
+
+$(document).ready(function(){
+    init();
+
+})
